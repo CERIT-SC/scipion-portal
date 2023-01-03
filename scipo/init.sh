5e40011d5b7c5d777a1d775cda2c54d7c448bff6
Josef Handl <handljosef@gmail.com>
2023-01-03 15:52:22 +0000

    add helm-chart to deploy the SP, old k8s yamls moved to the chart

--- scipo/init.sh
+++ scipo/init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -xe
 
 cd /srv/scipo
 python3 manage.py collectstatic
@@ -8,4 +9,4 @@ python3 manage.py migrate sessions
 python3 manage.py migrate auth
 
 #python3 manage.py runserver_plus --cert-file /mnt/cert/fullchain.pem --key-file /mnt/cert/privkey.pem 0.0.0.0:443
-python3 manage.py runserver 0.0.0.0:80
+python3 manage.py runserver 0.0.0.0:8080
