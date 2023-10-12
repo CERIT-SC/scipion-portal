2cc9509324438aee2625eb97313287d12f45d12a
Josef Handl <handljosef@gmail.com>
2023-10-12 15:24:19 +0200

    improve bootstrap table custom js

--- scipo/web/static/assets/js/demo.js
+++ scipo/web/static/assets/js/demo.js
@@ -7,18 +7,6 @@
 
 /* eslint-disable camelcase */
 
-$(document).ready(function () {
-  $table = $("table.data-table");
-    $table.bootstrapTable({
-        pagination: true,
-        sidePagination: "server",
-        sortable: true,
-        sortReset: true,
-        filterControl: true
-    })
-});
-
-
 (function ($) {
   'use strict'
 
