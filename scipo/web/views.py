9037dceaf5b9bbab557b0f35959cae8b1a218e4e
Josef Handl <handljosef@gmail.com>
2023-10-13 14:14:27 +0200

    dynamic table prototype with real data - spaces

--- scipo/web/views.py
+++ scipo/web/views.py
@@ -16,6 +16,8 @@ from .api.helm import Helmctl
 logger = logging.getLogger('django')
 
 
+logger = logging.getLogger('django')
+
 def oidc_callback(request):
     return HttpResponse("Hello, world. You're at the portal index.")
 
@@ -47,21 +49,6 @@ def instance_list(request):
 
 @login_required(login_url="/oidc/authenticate/")
 def project_list(request):
-    # OIDC token received from authentication
-    oidc_token = request.session.get('oidc_access_token')
-
-    data_spaces, error = Datahub.list_spaces(oidc_token)
-
-    spaces = list()
-    if not error:
-        for space_id in data_spaces['spaces']:
-            space_data, e = Datahub.get_space(oidc_token, space_id)
-
-            spaces.append({
-                'name': '' if e else space_data['name'],
-                'space_id': space_data['spaceId']
-            })
-
     site_labels = {
         'header': 'Projects',
         'create_space_btn': 'Project Space',
@@ -72,27 +59,11 @@ def project_list(request):
     return render(request, "space-list.html",
         context = {
             'site_labels': site_labels,
-            'available_spaces': spaces,
-            'info': "null",
+            'info': 'null',
         })
 
 @login_required(login_url="/oidc/authenticate/")
 def dataset_list(request):
-    # OIDC token received from authentication
-    oidc_token = request.session.get('oidc_access_token')
-
-    data_spaces, error = Datahub.list_spaces(oidc_token)
-
-    spaces = list()
-    if not error:
-        for space_id in data_spaces['spaces']:
-            space_data, e = Datahub.get_space(oidc_token, space_id)
-
-            spaces.append({
-                'name': '' if e else space_data['name'],
-                'space_id': space_data['spaceId']
-            })
-
     site_labels = {
         'header': 'Datasets',
         'create_space_btn': 'Dataset Space',
@@ -103,8 +74,7 @@ def dataset_list(request):
     return render(request, "space-list.html",
         context = {
             'site_labels': site_labels,
-            'available_spaces': spaces,
-            'info': "null",
+            'info': 'null',
         })
 
 @login_required(login_url="/oidc/authenticate/")
@@ -155,15 +125,26 @@ def create_instance_create(request):
                       'passwd': str(error)
                   })
 
-@login_required(login_url="/oidc/authenticate/")
-def create_namespace(request):
-    kubectl.create_namespace("frantaflinta")
-    return dataset_list(request)
-
 @login_required()
 def api_spaces(request):
-    result = """
-    [{"space_id": "vf08945d3", "name": "5203B", "project": "BALKAN", "subject": "WICK11550 John", "subject__space_id": "s90e46f7f", "subject__birth_date": "1991-01-01", "date": "2022-06-17", "subject__personal_ID": "9101010000", "subject_intials": "J.W.1991", "subject__gender": "F", "subject__handedness": "ns", "subject_age": 31, "comment": "ich", "quality_check": "NOT_OK", "number_of_sessions": 1, "checked": "un"}, {"space_id": "v127bc1ed", "name": "5204B", "project": "BALKAN", "subject": "WICK11550 John", "subject__space_id": "s90e46f7f", "subject__birth_date": "1991-01-01", "date": "2022-06-17", "subject__personal_ID": "9101010000", "subject_intials": "J.W.1991", "subject__gender": "F", "subject__handedness": "ns", "subject_age": 31, "comment": "", "quality_check": "NOT_OK", "number_of_sessions": 1, "checked": "un"}, {"space_id": "v98c295c2", "name": "test123", "project": "test", "subject": "WICK11550 John", "subject__space_id": "s90e46f7f", "subject__birth_date": "1991-01-01", "date": "2022-05-18", "subject__personal_ID": "9101010000", "subject_intials": "J.W.1991", "subject__gender": "F", "subject__handedness": "ns", "subject_age": 31, "comment": "aaa", "quality_check": "NOT_OK", "number_of_sessions": 1, "checked": "un"}, {"space_id": "vce5aec8d", "name": "763A", "project": "HealthBrainAge (HBA)", "subject": "WICK11550 John", "subject__space_id": "s90e46f7f", "subject__birth_date": "1991-01-01", "date": "2021-08-13", "subject__personal_ID": "9101010000", "subject_intials": "J.W.1991", "subject__gender": "F", "subject__handedness": "ns", "subject_age": 30, "comment": "", "quality_check": "NOT_OK", "number_of_sessions": 1, "checked": "un"}]
-    """
-    j = json.loads(result)
+    # OIDC token received from authentication
+    oidc_token = request.session.get('oidc_access_token')
+
+    data_spaces, error = Datahub.list_spaces(oidc_token)
+    logger.debug(f'data_spaces {str(data_spaces)}')
+
+    spaces = list()
+    if not error:
+        error = ''
+        for space_id in data_spaces['spaces']:
+            space_data, e = Datahub.get_space(oidc_token, space_id)
+
+            spaces.append({
+                'name': '' if e else space_data['name'],
+                'space_id': space_data['spaceId']
+            })
+
+            error += f' {str(e)}'
+
+    j = json.loads(json.dumps(spaces))
     return JsonResponse(j, safe=False)
