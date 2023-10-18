b5a46acfc557319370e3f4b6df936adc3bab407d
Josef Handl <handljosef@gmail.com>
2023-10-18 11:54:49 +0200

    instances - add link btn to the dynamic table

--- scipo/web/static/assets/js/dynamic-table.js
+++ scipo/web/static/assets/js/dynamic-table.js
@@ -1,11 +1,19 @@
 
 $(document).ready(function () {
     $table = $("table.data-table");
-      $table.bootstrapTable({
-          pagination: true,
-          sidePagination: "server",
-          sortable: true,
-          sortReset: true,
-          filterControl: true
-      })
-  });
+    $table.bootstrapTable({
+        pagination: true,
+        sidePagination: "server",
+        sortable: true,
+        sortReset: true,
+        filterControl: true
+    })
+});
+
+function linkFormatter(value) {
+    if (value) {
+        return `<a href="${value}" target="_blank"><i class="fas fa-link mr-1"></i>Open</a>`;
+    }
+
+    return ''
+}
