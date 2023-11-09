a59862a5da0386ca2252606b1523d10bf57803f3
Josef Handl <handljosef@gmail.com>
2023-11-09 10:46:30 +0100

    add health info from controller's rest api to the instances

--- scipo/web/static/assets/js/dynamic-table.js
+++ scipo/web/static/assets/js/dynamic-table.js
@@ -18,6 +18,18 @@ function linkFormatter(value) {
     return ''
 }
 
+function healthFormatter(value) {
+    if (value) {
+        if (value === "ok") {
+            return `<div class="health-success"><i class="fas fa-check mr-1"></i>${value}</div>`;
+        }
+
+        return `<div class="health-danger"><i class="fas fa-ban mr-1"></i>${value}</div>`
+    }
+
+    return `<div class="health-warning"><i class="fas fa-question mr-1"></i>unknown</div>`
+}
+
 function deleteFormatter(value) {
     if (value) {
         return `<form method="post" action="/instances/${value}"><button type="submit" class="data-table link-danger" target="_blank"><i class="fas fa-trash mr-1"></i>Delete</button></form>`;
