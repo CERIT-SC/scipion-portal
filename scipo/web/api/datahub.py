29529b033fc15e4fc357f9e3f103da355948784f
Josef Handl <handljosef@gmail.com>
2024-01-03 13:00:39 +0100

    add cache_machine

--- scipo/web/api/datahub.py
+++ scipo/web/api/datahub.py
@@ -4,21 +4,16 @@ import requests
 
 from datetime import datetime, timedelta
 
+from .cache_machine import CacheMachine
+
 
 logger = logging.getLogger('django')
 
 _host = 'https://datahub.egi.eu'
 
 class Datahubctl:
-
-    class CacheEntry:
-        def __init__(self, expiration, id, object) -> None:
-            self.expiration = expiration
-            self.id = id
-            self.object = object
-
-    cached_spaces = list()
-    cached_providers = list()
+    cached_spaces = CacheMachine()
+    cached_providers = CacheMachine()
 
     def _make_request_get(self, endpoint: str, oidc_token: str):
         headers = {'Authorization': f'Bearer egi:{oidc_token}'}
@@ -68,66 +63,20 @@ class Datahubctl:
 
     def get_space(self, oidc_token, space_id):
         # TODO it returns info about cached spaces without token verification!
+        def get_space_request():
+            result = self._make_request_get(f'/api/v3/onezone/spaces/{space_id}', oidc_token)
+            if not result:
+                logger.error(f"Obtaining info about space ID {space_id} failed.")
+            return result
 
-        # Check the cache entries
-        for cs in self.cached_spaces:
-            if cs.id != space_id:
-                continue
-
-            # space_id found in cache
-            if cs.expiration > datetime.now():
-                # Entry still actual > return content
-                return cs.object
-
-            # Entry expired > remove from cache
-            self.cached_spaces.remove(cs)
-            break
-
-        # Entry not found in cache or alredy expired
-        result = self._make_request_get(f'/api/v3/onezone/spaces/{space_id}', oidc_token)
-        if not result:
-            logger.error(f"Obtaining info about space ID {space_id} failed.")
-
-        # Save entry into the cache
-        self.cached_spaces.append(
-            self.CacheEntry(
-                datetime.now() + timedelta(hours=1),
-                space_id,
-                result
-            )
-        )
-
-        return result
+        return self.cached_spaces.obtain_value(space_id, get_space_request)
 
     def get_provider(self, oidc_token, provider_id):
         # TODO it returns info about cached providers without token verification!
+        def get_provider_request():
+            result = self._make_request_get(f'/api/v3/onezone/providers/{provider_id}', oidc_token)
+            if not result:
+                logger.error(f"Obtaining info about provider ID {provider_id} failed.")
+            return result
 
-        # Check the cache entries
-        for cs in self.cached_providers:
-            if cs.id != provider_id:
-                continue
-
-            # provider_id found in cache
-            if cs.expiration > datetime.now():
-                # Entry still actual > return content
-                return cs.object
-
-            # Entry expired > remove from cache
-            self.cached_providers.remove(cs)
-            break
-
-        # Entry not found in cache or alredy expired
-        result = self._make_request_get(f'/api/v3/onezone/providers/{provider_id}', oidc_token)
-        if not result:
-            logger.error(f"Obtaining info about provider ID {provider_id} failed.")
-
-        # Save entry into the cache
-        self.cached_providers.append(
-            self.CacheEntry(
-                datetime.now() + timedelta(hours=1),
-                provider_id,
-                result
-            )
-        )
-
-        return result
+        return self.cached_providers.obtain_value(provider_id, get_provider_request)
